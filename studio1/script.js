715603374b18ba04a740bc5ad3a33c503407435e
Kaela Han <kaehan@ucdavis.edu>
2018-01-25 14:36:05 -0800

    Mad Libs
    Add more divs (inputs).

--- studio1/script.js
+++ studio1/script.js
@@ -1,28 +1,40 @@
 use strict;
 
-// submit event
-document.f.onsubmit = processForm;
+console.log ('reading');
 
-//process function
+// capture submit event
+document.f.onsubmit = processForm;
+document.f.onreset = clearForm;
+
+//capture process function
 function processForm() {
 
-  //nouns
+console.log ('processing');
+
+  //store nouns in variables
   var n1 = document.f.n1.value;
   var n2 = document.f.n2.value;
   var n3 = document.f.n3.value;
   var n4 = document.f.n4.value;
   var n5 = document.f.n5.value;
+  var n6 = document.f.n6.value;
   var num = document.f.num.value;
 
-  //adjectives
+  //store adjectives in variables
   var a1 = document.f.a1.value;
   var a2 = document.f.a2.value;
+  var a3 = document.f.a3.value;
 
-  //verbs
+  //store verbs in variables
   var v1 = document.f.v1.value;
   var v2 = document.f.v2.value;
 
-  myMsg.innerHTML = "First, you preheat your " + n1 + " to " + num + " degrees. Then take a stick of " + n2 + " and melt it in a 10 inch cast- "
+  myMsg.innerHTML = "First, you preheat your " + n1 + " to " + num +
+  " degrees. Then take a stick of " + n2 + " and melt it in a 10 inch cast-" +
+  a1 + " pan. In a bowl, " + v1 + " granulated " + n3 + " & " + n4 " and " +v2
+  + " the mixture. Add milk and " + n5 + " beat together. Pour the mixture into a well-buttered " + 
+  n6 + " and place in the oven! Bake until a golden " +
+  a3 + " color. Bon Appetit!"
 
 }
 
