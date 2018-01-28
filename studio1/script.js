f4211bee1567bc3f4ecb84d48bf2b24e2958c1d9
Kaela Han <kaehan@ucdavis.edu>
2018-01-27 21:14:32 -0800

    Print MyMsg
    Print to console.

--- studio1/script.js
+++ studio1/script.js
@@ -2,10 +2,7 @@
 //use strict;
 
 console.log ('reading');
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
-//document.addEventListener("DOMContentLoaded", function(event) {
-//console.log("DOM fully loaded and parsed");
+
 
 // capture submit event
 document.f.onsubmit = processForm;
@@ -17,7 +14,7 @@ function processForm() {
   //store nouns in variables
   var n1 = document.f.n1.value;
   console.log(n1);
-  
+
   var n2 = document.f.n2.value;
   var n3 = document.f.n3.value;
   var n4 = document.f.n4.value;
@@ -35,12 +32,12 @@ function processForm() {
   var v2 = document.f.v2.value;
 
 //capture myMsg element
-  var myMsg = document.getElementById('myMsg');
+  var myMsg = document.getElementById("myMsg");
 
   //concatenate results
   myMsg.innerHTML = "First, you preheat your " + n1 + " to " + num + " degrees.Then take a stick of " + n2 + " and melt it in a 10 inch cast-" + a1 + " pan. In a bowl, " + v1 + " granulated " + n3 + " & " + n4 + " and " + v2 + " the mixture. Add milk and " + n5 + " beat together. Pour the mixture into a well-buttered " + n6 + " and place in the oven! Bake until a golden " + a2 + " color. Bon Appetit!";
   myMsg.className = "show";
-  console.log(myMsg);
+  console.log("myMsg");
 // prevent page from reloading
   return false;
 }
