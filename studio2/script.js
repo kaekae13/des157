89fc90cbf3ce6e5703e2a74acd02a51df04dbaf4
Kaela Han <kaehan@ucdavis.edu>
2018-02-01 18:49:18 -0800

    Styles
    Style mobile and desktop viewports.

---
+++ studio2/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log('reading');
+
+var d1=getElementById('d1');
+var d2=getElementById('d2');
+var d3=getElementById('d3');
+
+d1.addEventListener('click',function() {
+  d1.style.display='block';
+});
