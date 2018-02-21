1f1fecd1a3215c3b52ef61925aad818d92b1b6bc
Kaela Han <kaehan@ucdavis.edu>
2018-02-20 21:56:17 -0800

    Studio 4
    Update changes to studio 4.

--- studio4/script/script.js
+++ studio4/script/script.js
@@ -4,11 +4,12 @@ console.log("reading js");
 
 var song,
     loadSound,
-    fft,
-    amp,
-    spectrum,
-    y,
-    w;
+    analyzer;
+
+//var play = document.getElementById('play');
+//var pause = document.getElementById('pause');
+
+// load audio audioFile
 
 function preload() {
   song = loadSound("https://kaekae13.github.io/des157/studio4/audio/audioFile.mp3");
@@ -16,39 +17,47 @@ function preload() {
 
 function setup() {
   console.log('playing');
-
-  noFill();
-  var canvas = createCanvas(750,300);
+  var canvas = createCanvas(800,300);
   canvas.parent('waveContainer');
-
-
   song.loop();
   song.setVolume(0.1);
-
-  fft = new p5.FFT(0.9, 64);
-  amp = new p5.Amplitude();
-
-  w = width/64;
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(song);
 }
 
+//pause and play events
+//pause.onclick = function(event) {
+  //song.pause();
+  //play.className = "show";
+  //pause.className = "hide";
+//}
+
+//play.onclick = function (event) {
+  //song.play();
+  //play.className = "hide";
+  //pause.className = "show";
+//}
 
 
+// volume events
+//increase.onclick = function(event) {
+  //song.volume+=0.1;
+//}
 
-
+//decrease.onclick = function(event) {
+  //song.volume-=0.1;
+//}
 
 function draw() {
-
+  //console.log('audiovisualizer');
+  fill('#EFC2CE');
   noStroke();
-  fill(255);
+  var rms = analyzer.getLevel();
+  for (var i = 0; i< 800; i++){
+    canvas.rect(i, 300, 10, 50+rms*100);
+  }
+//ellipse(i-50, 300, 50,50);
+//rect (i-250,240,10, 50);
 
-  //var level = amp.getLevel();
-  var spectrum = fft.analyze();
-
-
-  for (var i = 0; i< spectrum.length; i+=2) {
-    var amp = spectrum[i];
-    var y =  map(amp, 0, 250, height, 0);
-    rect(i * w , y, w - 2, height * 2 - y);
-}
-
-}
+ }
+//}
