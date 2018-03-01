18f622d70749959bb5fd91507cd33f20f49a35e4
Kaela Han <kaehan@ucdavis.edu>
2018-03-01 10:41:20 -0800

    Add interactive Wireframe
    Add interactive wireframe.

---
+++ project/int-wireframe/musicPlayer/script2.js
@@ -0,0 +1,11 @@
+var dropDown = document.getElementById ('dropDown');
+var body = document.getElementById ('body');
+var colorMenu = document.getElementById('colors');
+
+dropDown.addEventListener ('click', function() {
+
+
+colors.className = "fade";
+colors.style.opacity = '1';
+
+});
--- project/int-wireframe/script.js
+++ project/int-wireframe/script.js
@@ -1,223 +1,59 @@
 "use strict";
+var title = document.getElementById('title');
+var btnNxt = document.getElementById('nxtPage');
 
-console.log("reading js");
+var about = document.getElementById('about');
+var btnNxt1 = document.getElementById('nxtPage1');
+var btnBack1 = document.getElementById('goBack1');
+var title1=document.getElementById('title1');
+var content = document.getElementById ('content');
 
-var song,
-    fft,
-    w,
-    amp;
+var instruct = document.getElementById('instructions');
+var btnBack2 = document.getElementById('goBack2');
+var title2=document.getElementById('title2');
+var steps=document.getElementById('steps');
 
+btnNxt.addEventListener('click', function() {
+  console.log('reading');
+  title.className= "disappear";
+  title.style.opacity = '0';
 
-var play = document.getElementById('play');
-var pause = document.getElementById('pause');
+  btnNxt.className = "disappear";
+  btnNxt.style.opacity="0";
 
-var increase = document.getElementById('increase');
-var decrease = document.getElementById('decrease');
+  about.className="fade";
+  about.style.opacity = '1';
 
 
+});
 
+btnNxt1.addEventListener('click', function() {
+  console.log('reading');
+  about.className="disappear";
+  about.style.opacity = '0';
 
-// colorPicker
+  instruct.className="fade";
+  instruct.style.opacity="1";
+});
 
-var color1 = document.getElementById('color1');
-var color2 = document.getElementById('color2');
-var color3 = document.getElementById('color3');
-var color4 = document.getElementById('color4');
-var color5 = document.getElementById('color5');
-var color6 = document.getElementById('color6');
-var color7 = document.getElementById('color7');
-var color8 = document.getElementById('color8');
-var color9 = document.getElementById('color9');
-var color10 = document.getElementById('color10');
-var color11 = document.getElementById('color11');
-var color12 = document.getElementById('color12');
-var color13 = document.getElementById('color13');
-var color14 = document.getElementById('color14');
 
+btnBack1.addEventListener('click', function() {
+  console.log('reading');
+  about.className="disappear";
+  about.style.opacity = '0';
 
-var colorChoices= [
-  '#FD8BD9',
-  '#ffd1ff',
-  '#ffafbd',
-  '#fcb69f',
-  '#fddb92',
-  '#f9f586',
-  '#92fe9d',
-  '#2af598',
-  '#00cdac',
-  '#00f2fe',
-  '#00c6fb',
-  '#4facfe',
-  '#cd9cf2',
-  '#d57eeb'
-];
+  title.className="fade";
+  title.style.opacity="1";
 
+  btnNxt.className = "fade";
+  btnNxt.style.opacity="1";
+});
 
-// visualizer buttons
+btnBack2.addEventListener ('click', function() {
+  console.log('reading');
+  instruct.className='disappear';
+  instruct.style.opacity = '0';
 
-var bar = document.getElementById('bar');
-var wave = document.getElementById('wave');
-var radial = document.getElementById('radial');
-
-
-// load audio
-
-function preload() {
-  song = loadSound( "https://kaekae13.github.io/des157/studio4/audio/audioFile.mp3");
-  }
-
-
-function setup() {
-  console.log('playing');
-  var canvas = createCanvas(1600,360);
-  canvas.parent('barContainer');
-  var canvas1 = createCanvas(1600,300);
-  canvas1.parent('waveContainer');
-
-  song.loop();
-  song.setVolume(0.1);
-
-  fft = new p5.FFT(0.9, 128);
-  fft.setInput(song);
-
-  amp = new p5.Amplitude();
-
-  fill(colorChoices[10]);
-  w = width/64;
-}
-
-
-
-// pause and play events
-
-pause.onclick = function(event) {
-  song.pause();
-  play.className = "btnShow";
-  pause.className = "hide";
-}
-
-play.onclick = function (event) {
-  song.play();
-  play.className = "hide";
-  pause.className = "btnShow";
-}
-
-
-// volume events
-increase.onclick = function(event) {
-  song.amp+=0.2;
-}
-
-decrease.onclick = function(event) {
-  song.amp-=0.2;
-}
-
-
-
-// create separate function for sound bars
-function bars() {
-    var spectrum = fft.analyze();
-    stroke('white');
-    strokeWeight(6);
-
-    // eventlisteners for color buttons
-
-    color1.addEventListener('click', function() {
-      fill(colorChoices[0]);
-    });
-
-    color2.addEventListener('click', function() {
-      fill(colorChoices[1]);
-    });
-
-    color3.addEventListener('click', function() {
-      fill(colorChoices[2]);
-    });
-
-    color4.addEventListener('click', function() {
-      fill(colorChoices[3]);
-    });
-
-    color5.addEventListener('click', function() {
-      fill(colorChoices[4]);
-    });
-
-    color6.addEventListener('click', function() {
-      fill(colorChoices[5]);
-    });
-
-    color7.addEventListener('click', function() {
-      fill(colorChoices[6]);
-    });
-
-    color8.addEventListener('click', function() {
-      fill(colorChoices[7]);
-    });
-
-    color9.addEventListener('click', function() {
-      fill(colorChoices[8]);
-    });
-
-    color10.addEventListener('click', function() {
-      fill(colorChoices[9]);
-    });
-
-    color11.addEventListener('click', function() {
-      fill(colorChoices[10]);
-    });
-
-    color12.addEventListener('click', function() {
-      fill(colorChoices[11]);
-    });
-
-    color13.addEventListener('click', function() {
-      fill(colorChoices[12]);
-    });
-
-    color14.addEventListener('click', function() {
-      fill(colorChoices[13]);
-    });
-
-
-
-    for (var i = 0; i< spectrum.length; i++) {
-        var x = map(i, 0, spectrum.length, 0, width);
-        var h = -height + map(spectrum[i], 0, 255, height, 0);
-        rect (x + w, height,  width / spectrum.length, h);
-      }
-  }
-
-
-function waves() {
-  var spectrum = fft.analyze();
-  noFill();
-  beginShape();
-  stroke(colorChoices[3]);
-  strokeWeight(1);
-  for (var i = 0; i< spectrum.length; i++){
-    var x = map(i, 0, spectrum.length, 0, width);
-    var y = map( spectrum[i], -1, 1, 0, height);
-    vertex(x,y);
-  }
-  endShape();
-}
-
-
-
-function draw() {
-  bars();
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-// add event listeners to change color
+  about.className='fade';
+  about.style.opacity = '1';
+})
