58b693f08bdfa0afccd2a82fba962a8503776310
Kaela Han <kaehan@ucdavis.edu>
2018-03-12 22:45:25 -0700

    Proto 3
    Script .

--- project/proto3/musicPlayer/script.js
+++ project/proto3/musicPlayer/script.js
@@ -2,8 +2,6 @@
 
 console.log("reading js");
 
-window.onload(alert('Click on a color to begin! *Site is under construction*'));
-
 var song,
     fft,
     w,
@@ -61,7 +59,7 @@ var bar = document.getElementById('bar');
 var wave = document.getElementById('wave');
 var radial = document.getElementById('radial');
 
-  var buttonClick="barClick";
+
 // load audio
 
 function preload() {
@@ -88,43 +86,10 @@ function setup() {
 
 
   w = width/64;
-
-
 }
 
-function waves() {
-    var spectrum=fft.analyze();
-    beginShape();
-    smooth();
-    noFill();
-    strokeWeight(3);
-    for (var i = 0; i<spectrum.length; i++) {
-      var x = map(i, 0, spectrum.length, 0, width);
-      var h = map(spectrum[i], 0, 255, height-50, 0);
-      vertex(i+x, h);
-      clear();
-    }
-    endShape();
-  }
-
-
-function bars() {
-    noStroke();
-    var spectrum = fft.analyze();
-    for (var i = 0; i< spectrum.length; i++) {
-        var x = map(i, 0, spectrum.length, 0, width);
-        var h = -height + map(spectrum[i], 0, 255, height, 0);
-        rect (x + w, height,  width / spectrum.length - 3, h);
-      }
-    }
-
-
-function radial() {
-    var spectrum=fft.analyze();
-    translate(width/2, height/2);
-    for (var i=0; i<spectrum.length; i++) {
-      ellipse(0,0,spectrum[i] *50, spectrum[i]*50);
-    }
+chooseAudio.onclick= function() {
+  song.stop();
 }
 
 // pause and play events
@@ -227,24 +192,64 @@ decrease.onclick = function(event) {
       stroke(colorChoices[13]);
     });
 
-    bar.addEventListener('click', function() {
-      var buttonClick = "barClick";
-    });
 
-    wave.addEventListener ('click', function() {
-      var buttonClick = "waveClick";
-    });
 
-    radial.addEventListener ('click', function() {
-    var buttonClick = "radialClick";
-    });
 
-    chooseAudio.addEventListener('click', function() {
-    song.stop();
-    });
+
+
+  function waves() {
+      var spectrum=fft.analyze();
+      beginShape();
+      smooth();
+      noFill();
+      strokeWeight(3);
+      for (var i = 0; i<spectrum.length; i++) {
+        var x = map(i, 0, spectrum.length, 0, width);
+        var h = map(spectrum[i], 0, 255, height-50, 0);
+        vertex(i+x, h);
+        clear();
+      }
+      endShape();
+    }
+
+
+  function bars() {
+      noStroke();
+      var spectrum = fft.analyze();
+      for (var i = 0; i< spectrum.length; i++) {
+          var x = map(i, 0, spectrum.length, 0, width);
+          var h = -height + map(spectrum[i], 0, 255, height, 0);
+          rect (x + w, height,  width / spectrum.length - 3, h);
+        }
+      }
+
+
+  function radial() {
+      var spectrum=fft.analyze();
+      translate(width/2, height/2);
+      for (var i=0; i<spectrum.length; i++) {
+        ellipse(0,0,spectrum[i] *50, spectrum[i]*50);
+      }
+  }
+
 
 function draw() {
   background('white');
+  var buttonClick=" ";
+
+  bar.onclick = function(event) {
+    var buttonClick = "barClick";
+
+  }
+
+  wave.onclick = function(event) {
+    var buttonClick = "waveClick";
+
+  }
+
+radial.onclick = function(event) {
+  var buttonClick = "radialClick";
+  }
 
   console.log(buttonClick);
 
@@ -267,14 +272,3 @@ function draw() {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-// add event listeners to change color
